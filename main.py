f0add01c2488fcc056bd17b038231323d755b4c3
Harrysibbenga <sibbengaharry@gmail.com>
2024-10-18 11:02:59 +0100

    Added vlc class functionality now testing

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from Controller import Controls
 def main():
     """Main loop of the music player."""
     # Initialize the LCD Class
-    lcd_inst = LCDClass()
     time_constant = 5
+    lcd_inst = LCDClass(time_constant)
 
     if lcd_inst.image is None:  # Check if initialization failed
         print("Error initializing LCD. Exiting...")
@@ -17,14 +17,14 @@ def main():
             media_choice = input("Choose media player (vlc or spotify): ").lower()
             if media_choice == "vlc":
                 from VLCMedia import VLCPlayer
-                media_player_inst = VLCPlayer()
+                media_player_inst = VLCPlayer(time_constant)
                 media_player_inst.load_music("/home/rpi_speaker/Music")
                 lcd_inst.display_message("You have picked VLC")
                 time.sleep(time_constant)
                 break
             elif media_choice == "spotify":
                 from SpotifyMedia import SpotifyPlayer
-                media_player_inst = SpotifyPlayer()
+                media_player_inst = SpotifyPlayer(time_constant)
                 lcd_inst.display_message("You have picked SPOTIFY")
                 time.sleep(time_constant)
                 break
@@ -39,7 +39,7 @@ def main():
         lcd_inst.display_message("Your device is ready !!")
         time.sleep(5)
 
-        controls = Controls(media_player_inst, lcd_inst)  # Create a Controls instance
+        controls = Controls(media_player_inst, lcd_inst, time_constant)  # Create a Controls instance
         controls.start()  # Start the keyboard listener
 
         while controls.running:  # Main loop
@@ -57,7 +57,7 @@ def main():
     finally:
         print("Resetting display and ending program")
         lcd_inst.display_message("Goodbye. Untill next time :)")
-        time.sleep(5)
+        time.sleep(time_constant)
         lcd_inst.disp.fill(0)  # Clear the display
         lcd_inst.disp.show()
 
